1bd0ad001dc9f1e49e3a4dd5dccb4a1d8d945223
daniel georgiev <danzgeorg.contact@gmail.com>
2026-08-21 17:29:12 +0100

    add @timer decorator with functools.wraps still stuck on task 3 of Issue #3

---
+++ decorators.py
@@ -0,0 +1,22 @@
+import functools
+import time
+
+#def retry(times):
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__}() in {run_time:.4f} secs")
+        return value
+    return wrapper
+#return timer
+
+@timer
+def example():
+    time.sleep(1)
+
+print(example.__name__)
